e8606595efbe35bfaadc6c32b3b2684f00db949f
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-07-06 16:19:22 -0600

    mobile animation and interactive map

--- portfolio_react/src/Contact/Contact.jsx
+++ portfolio_react/src/Contact/Contact.jsx
@@ -29,7 +29,7 @@ class Contact extends Component {
                         <a target="_blank" rel="noopener noreferrer" href='https://www.facebook.com/adamwolfman'>facebook.com/adamwolfman</a>
                     </div>
                 
-                    <img src={require('../images/denver_map.png')} alt="map"/>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24556.690763213086!2d-105.02619480965761!3d39.70400368505795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7f72ea9ddb97%3A0x9ffda6584f0562c1!2sAthmar+Park%2C+Denver%2C+CO!5e0!3m2!1sen!2sus!4v1562449100490!5m2!1sen!2sus" title="map" width="600" height="450" frameBorder="0"  allowFullScreen></iframe>
                 
                 </div>
                 

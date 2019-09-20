7f18a739419a9a9b9a2028a7e20f9a78faf067f2
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-09-20 10:49:57 -0600

    updated about and resume

--- portfolio_react/src/About/About.jsx
+++ portfolio_react/src/About/About.jsx
@@ -13,21 +13,12 @@ class About extends Component {
             <div id='about' className='about'>
                 <h1>Stuff I Am</h1>
                 <div>
-                    <p>I am a software engineer, recently graduated from 
-                    the General Assembly 12 week immersive program with 
-                    8 years experience in mortgage finance as an analyst. </p>
-
-                    <p>In my last role I worked as the point of contact across 
-                    agencies. I would analyze customer data and come up with 
-                    solutions that fit within product regulations to approve deals 
-                    before their closing deadlines. While I enjoyed breaking down 
-                    big problems in to smaller pieces to come up with solutions, 
-                    I felt I was missing out on actually creating things, which is what 
-                    led me to pivot and pursue a change in to coding.</p>
-
-                    <p>I am really excited to take the skills I have started to build at GA 
-                    and rocket them to the next level going forward. I am interested 
-                    most in clean design, both in code and user experience.</p>
+                    <p>I am a Web Developer, Marketing major, and graduate of General Assembly Software 
+                        Engineering Immersive program. I've designed and built full-stack apps, but specialize in front end development
+                        using Javascript frameworks such as React and Angular. Recently I've been excited about serverless tech,
+                        releasing a JAM stack with an ecommerce integration. If you have a project idea or need a website refresh
+                        please reach out for a free consultation! 
+                    </p>
                 </div>
             </div>
         )
